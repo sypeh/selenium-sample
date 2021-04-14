74b0a0317a880479f2e341c76f38199d7adf5ece
Shumpei Yokoyama <yokoyama.s2@pol-japan.co.jp>
2021-04-15 08:54:30 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # selenium-sample
+selenium gridの実行環境を用意するリポジトリ。
+pythonでテスト用のスクリプトを書いていきます。
+
+# 使い方
+dockerを立ち上げます。
+```
+cd docker
+docker-compose build
+docker-compose up -d
+```
+
+pythonコンテナに入る
+```
+docker exec -it python /bin/bash
+```
+
+sampleプログラムを実行
+```
+cd tests/
+python sample.py 
+```
+
+# selenium browserで動きを確認する
+vnc://localhost:55000 にパスワード `secret` で接続し、sampleプログラムを実行
---
+++ tests/sample.py
@@ -0,0 +1,35 @@
+import time
+SELENIUM_HUB = 'http://selenium-hub:4444/wd/hub'
+
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+driver = webdriver
+
+try:
+
+    print("starting driver")
+    driver = webdriver.Remote(
+      command_executor=SELENIUM_HUB,
+      desired_capabilities=DesiredCapabilities.CHROME,
+    )
+
+    print("opening the page")
+    driver.get('https://www.google.com/')
+
+    print("take a screenshot")
+    driver.save_screenshot('screenshot.png')
+
+    print("show title")
+    print(driver.title)
+
+    time.sleep(2)
+
+except:
+
+    print("error")
+
+finally:
+
+    driver.quit()
+    print("finish")
